bea861d2627898db58ef78e4c07fc77eae88bb38
Zsolt Pete <pete.zsolt@codeyard.eu>
2017-09-25 21:54:22 +0200

    ✨ Translated button added

--- PokeMeApp/PokeMeApp/Utilities/Extensions/CYTranslatedButton.swift
+++ PokeMeApp/PokeMeApp/Utilities/Extensions/CYTranslatedButton.swift
@@ -9,13 +9,11 @@
 import UIKit
 
 class CYTranslatedButton: UIButton {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    @IBInspectable var translationKey: String = "" {
+        didSet {
+            self.setTitle(translationKey.localized, for: .normal)
+        }
     }
-    */
 
 }
