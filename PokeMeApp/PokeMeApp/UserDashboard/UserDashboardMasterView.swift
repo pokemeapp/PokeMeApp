613512f4c9d0b60ce624acbb5f11171d0d8a9f52
Zsolt Pete <pete.zsolt@codeyard.eu>
2017-11-03 21:37:50 +0100

    ➕ Added new files

---
+++ PokeMeApp/PokeMeApp/UserDashboard/UserDashboardMasterView.swift
@@ -0,0 +1,21 @@
+//
+//  UserDashboardMasterView.swift
+//  PokeMeApp
+//
+//  Created by Zsolt Pete on 2017. 11. 03..
+//  Copyright © 2017. Zsolt Pete. All rights reserved.
+//
+
+import UIKit
+
+class UserDashboardMasterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
