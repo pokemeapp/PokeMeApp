5a09710301f0a40623f0bf0a4f01d10685e42823
Zsolt Pete <pete.zsolt@codeyard.eu>
2017-11-13 23:15:14 +0100

    ➕ Added new files

---
+++ PokeMeApp/PokeMeApp/NewUserHabit/NewHabitHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  NewHabitHeaderView.swift
+//  PokeMeApp
+//
+//  Created by Zsolt Pete on 2017. 11. 13..
+//  Copyright © 2017. Zsolt Pete. All rights reserved.
+//
+
+import UIKit
+
+class NewHabitHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
