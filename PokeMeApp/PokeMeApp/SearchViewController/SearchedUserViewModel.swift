8a12f159edc3116de248b0158ae58576e395cf53
Zsolt Pete <pete.zsolt@codeyard.eu>
2017-11-14 21:34:15 +0100

    ➕ Added new files

---
+++ PokeMeApp/PokeMeApp/SearchViewController/SearchedUserViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SearchedUserViewModel.swift
+//  PokeMeApp
+//
+//  Created by Zsolt Pete on 2017. 11. 14..
+//  Copyright © 2017. Zsolt Pete. All rights reserved.
+//
+
+import UIKit
+
+class SearchedUserViewModel: NSObject {
+
+}
