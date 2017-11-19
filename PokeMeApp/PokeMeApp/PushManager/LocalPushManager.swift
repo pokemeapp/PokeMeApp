c0d39a55398d82ea2d495ede6523f5e76bbc2c96
Zsolt Pete <pete.zsolt@codeyard.eu>
2017-11-19 23:59:34 +0100

    ➕ Added new files

---
+++ PokeMeApp/PokeMeApp/PushManager/LocalPushManager.swift
@@ -0,0 +1,13 @@
+//
+//  LocalPushManager.swift
+//  PokeMeApp
+//
+//  Created by Zsolt Pete on 2017. 11. 19..
+//  Copyright © 2017. Zsolt Pete. All rights reserved.
+//
+
+import UIKit
+
+class LocalPushManager: NSObject {
+
+}
---
+++ PokeMeApp/PokeMeApp/Utilities/Extensions/UIView+ParentController.swift
@@ -0,0 +1,9 @@
+//
+//  UIView+ParentController.swift
+//  PokeMeApp
+//
+//  Created by Zsolt Pete on 2017. 11. 19..
+//  Copyright © 2017. Zsolt Pete. All rights reserved.
+//
+
+import Foundation
