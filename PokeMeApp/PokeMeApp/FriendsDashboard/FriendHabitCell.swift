ca5485ff36acd41ad8c51fcc9fa3c03557b5bb0a
Zsolt Pete <pete.zsolt@codeyard.eu>
2017-11-19 21:29:30 +0100

    ➕ Added new files

---
+++ PokeMeApp/PokeMeApp/FriendsDashboard/FriendHabitCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendHabitCell.swift
+//  PokeMeApp
+//
+//  Created by Zsolt Pete on 2017. 11. 19..
+//  Copyright © 2017. Zsolt Pete. All rights reserved.
+//
+
+import UIKit
+
+class FriendHabitCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
