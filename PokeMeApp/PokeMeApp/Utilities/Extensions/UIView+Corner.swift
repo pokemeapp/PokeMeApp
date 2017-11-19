ae85d55b848a7d53b18fdd28c965a357a523c8dd
Zsolt Pete <pete.zsolt@codeyard.eu>
2017-11-19 23:01:53 +0100

    ✨ Corner extension added

--- PokeMeApp/PokeMeApp/Utilities/Extensions/UIView+Corner.swift
+++ PokeMeApp/PokeMeApp/Utilities/Extensions/UIView+Corner.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIView {
+    
+    public func roundCorners(corners:UIRectCorner, radius: CGFloat) {
+        self.layer.cornerRadius = 0.0
+        DispatchQueue.main.async {
+            let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+            let mask = CAShapeLayer()
+            mask.path = path.cgPath
+            self.layer.mask = mask
+        }
+    }
+    
+}
