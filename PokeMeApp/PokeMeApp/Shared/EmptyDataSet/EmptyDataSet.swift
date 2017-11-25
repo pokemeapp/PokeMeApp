012351d81cdae8518b3ae38aadb4911958df28cf
Zsolt Pete <pete.zsolt@codeyard.eu>
2017-11-25 22:54:25 +0100

    ✨ Implemented empty da

--- PokeMeApp/PokeMeApp/Shared/EmptyDataSet/EmptyDataSet.swift
+++ PokeMeApp/PokeMeApp/Shared/EmptyDataSet/EmptyDataSet.swift
@@ -10,12 +10,54 @@ import UIKit
 
 class EmptyDataSet: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    var title: String? {
+        didSet {
+            self.titleLabel.text = title
+        }
     }
-    */
-
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.loadXib()
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        self.layer.cornerRadius = 3.0
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.loadXib()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.loadXib()
+    }
+    
+    func loadXib() {
+        guard let view = loadViewFromNib() else { return }
+        view.frame = bounds
+        view.autoresizingMask =
+            [.flexibleWidth, .flexibleHeight]
+        addSubview(view)
+    }
+    
+    func loadViewFromNib() -> UIView? {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: "EmptyDataSet", bundle: bundle)
+        return nib.instantiate(
+            withOwner: self,
+            options: nil).first as? UIView
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.loadXib()
+    }
+    
 }
