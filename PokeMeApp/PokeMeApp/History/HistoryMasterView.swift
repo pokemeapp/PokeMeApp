f74cd4e552f49195476c85f92fa75fbffa3872bb
Zsolt Pete <pete.zsolt@codeyard.eu>
2017-11-26 19:49:11 +0100

    ➕ Added new files

---
+++ PokeMeApp/PokeMeApp/History/HistoryMasterView.swift
@@ -0,0 +1,21 @@
+//
+//  HistoryMasterView.swift
+//  PokeMeApp
+//
+//  Created by Zsolt Pete on 2017. 11. 26..
+//  Copyright © 2017. Zsolt Pete. All rights reserved.
+//
+
+import UIKit
+
+class HistoryMasterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ PokeMeApp/PokeMeApp/History/UserHistoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserHistoryCell.swift
+//  PokeMeApp
+//
+//  Created by Zsolt Pete on 2017. 11. 26..
+//  Copyright © 2017. Zsolt Pete. All rights reserved.
+//
+
+import UIKit
+
+class UserHistoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
