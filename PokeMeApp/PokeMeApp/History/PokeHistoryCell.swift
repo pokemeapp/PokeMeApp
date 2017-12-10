bbcdd138053b14efa24378431ef4fd1b8f0b0f38
Zsolt Pete <pete.zsolt@codeyard.eu>
2017-12-10 18:14:18 +0100

    ➕ Added new files

---
+++ PokeMeApp/PokeMeApp/History/PokeHistoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokeHistoryCell.swift
+//  PokeMeApp
+//
+//  Created by Zsolt Pete on 2017. 12. 10..
+//  Copyright © 2017. Zsolt Pete. All rights reserved.
+//
+
+import UIKit
+
+class PokeHistoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ PokeMeApp/PokeMeApp/History/PokeHistoryViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PokeHistoryViewModel.swift
+//  PokeMeApp
+//
+//  Created by Zsolt Pete on 2017. 12. 10..
+//  Copyright © 2017. Zsolt Pete. All rights reserved.
+//
+
+import UIKit
+
+class PokeHistoryViewModel: NSObject {
+
+}
