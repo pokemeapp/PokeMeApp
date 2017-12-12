7fd4877687ab850fa60fc7d7b005ebca439bf829
Zsolt Pete <pete.zsolt@codeyard.eu>
2017-12-12 18:48:14 +0100

    🚧 Working on local push

--- PokeMeApp/PokeMeApp/PushManager/LocalPushManager.swift
+++ PokeMeApp/PokeMeApp/PushManager/LocalPushManager.swift
@@ -29,5 +29,46 @@ class LocalPushManager {
         }
         print("Local push sent!")
     }
+    
+    func setUpLocalNotification(message: String, hm: String) {
+        
+        
+        var array = hm.split(separator: ":")
+        let hour = array[0]
+        let minute = array[1]
+        let hourInt = Int(hour)!
+        let minuteInt = Int(minute)!
+        
+        // have to use NSCalendar for the components
+        let calendar = NSCalendar(identifier: .gregorian)!;
+        
+        var dateFire = Date()
+        
+        // if today's date is passed, use tomorrow
+        var fireComponents = calendar.components( [NSCalendar.Unit.day, NSCalendar.Unit.month, NSCalendar.Unit.year, NSCalendar.Unit.hour, NSCalendar.Unit.minute], from:dateFire)
+        
+        if (fireComponents.hour! > hourInt
+            || (fireComponents.hour == hourInt && fireComponents.minute! >= minuteInt) ) {
+            
+            dateFire = dateFire.addingTimeInterval(86400)  // Use tomorrow's date
+            fireComponents = calendar.components( [NSCalendar.Unit.day, NSCalendar.Unit.month, NSCalendar.Unit.year, NSCalendar.Unit.hour, NSCalendar.Unit.minute], from:dateFire);
+        }
+        
+        // set up the time
+        fireComponents.hour = hourInt
+        fireComponents.minute = minuteInt
+        
+        // schedule local notification
+        dateFire = calendar.date(from: fireComponents)!
+        
+        let localNotification = UILocalNotification()
+        localNotification.fireDate = dateFire
+        localNotification.alertBody = message
+        localNotification.repeatInterval = NSCalendar.Unit.day
+        localNotification.soundName = UILocalNotificationDefaultSoundName;
+        
+        UIApplication.shared.scheduleLocalNotification(localNotification);
+        
+    }
 
 }
